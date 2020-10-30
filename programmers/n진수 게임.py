a7bfb4780b939492fa948596b755169ad9028a35
kseungwoo <seungwookim.dev@gmail.com>
2020-10-30 14:30:11 +0900

    Update n진수 게임.py

--- programmers/n진수 게임.py
+++ programmers/n진수 게임.py
@@ -1,4 +1,4 @@
-# 문제 출처 : https://programmers.co.kr/learn/courses/30/lessons/17687?language=python3
+# 출처 : https://programmers.co.kr/learn/courses/30/lessons/17687?language=python3
 
 NOTE = '0123456789ABCDEF'
 def change(num, base):
