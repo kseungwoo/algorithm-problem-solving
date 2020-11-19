b8b97a2e2fbe6ce84fe1370c099f4fcea883e7ae
kseungwoo <seungwookim.dev@gmail.com>
2020-11-19 14:13:11 +0900

    Create 최고의 집합.py

---
+++ programmers/level3/최고의 집합.py
@@ -0,0 +1,13 @@
+# 출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
+def solution(n, s):
+    answer = []
+    element = s // n
+    if element == 0:
+        return [-1]
+    for _ in range(n):
+        answer.append(element)
+    index = -1
+    for _ in range(s % n):
+        answer[index] += 1
+        index -= 1
+    return answer
