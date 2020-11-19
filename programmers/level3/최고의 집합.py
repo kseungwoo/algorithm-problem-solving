f769918c1d89f6bc4dd4de7d9f5ceff4b247bb9e
kseungwoo <seungwookim.dev@gmail.com>
2020-11-19 14:18:32 +0900

    Update 최고의 집합.py

--- programmers/level3/최고의 집합.py
+++ programmers/level3/최고의 집합.py
@@ -1,11 +1,9 @@
 # 출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
 def solution(n, s):
-    answer = []
     element = s // n
     if element == 0:
         return [-1]
-    for _ in range(n):
-        answer.append(element)
+    answer = [element for _ in range(n)]
     index = -1
     for _ in range(s % n):
         answer[index] += 1
