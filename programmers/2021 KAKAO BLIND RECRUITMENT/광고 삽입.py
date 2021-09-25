43e1f5178b8bd98ab49a3db59f143b54520fbd90
김승우 <seungwookim.dev@gmail.com>
2021-09-25 14:58:42 +0900

    [Update] 출처 남기기

--- programmers/2021 KAKAO BLIND RECRUITMENT/광고 삽입.py
+++ programmers/2021 KAKAO BLIND RECRUITMENT/광고 삽입.py
@@ -1,3 +1,4 @@
+# 출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
 def toSecond(s):
     s = s.split(':')
     return int(s[0]) * 3600 + int(s[1]) * 60 + int(s[2])
--- programmers/2021 KAKAO BLIND RECRUITMENT/카드 짝 맞추기.py
+++ programmers/2021 KAKAO BLIND RECRUITMENT/카드 짝 맞추기.py
@@ -1,5 +1,4 @@
 # 출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
-
 import copy
 from collections import deque
 
