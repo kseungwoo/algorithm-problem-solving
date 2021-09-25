edc8b84125bf226b7350a03ac964be6aab6a1b9c
김승우 <seungwookim.dev@gmail.com>
2021-09-25 18:59:09 +0900

    [Update] 경주로 건설

--- programmers/2020 카카오 인턴십/경주로 건설.py
+++ programmers/2020 카카오 인턴십/경주로 건설.py
@@ -1,17 +1,18 @@
 import copy
 import math
+from collections import deque
 
 
 def solution(board):
     n = len(board)
     cmap = [[[math.inf for _ in range(2)] for _ in range(n)] for _ in range(n)]
-    s = []
+    s = deque()
     dx = (0, 0, 1, -1)
     dy = (1, -1, 0, 0)
 
     s.append((0, 0, 0, -1))
     while s:
-        r, c, cost, prev = s.pop()
+        r, c, cost, prev = s.popleft()
         if r == n - 1 and c == n - 1:
             continue
         for d in range(4):
@@ -23,11 +24,11 @@ def solution(board):
                 cur = 1
             if 0 <= nr < n and 0 <= nc < n and board[nr][nc] == 0:
                 if prev == cur or prev == -1:
-                    if cost + 100 <= cmap[nr][nc][cur]:
+                    if cost + 100 < cmap[nr][nc][cur]:
                         s.append((nr, nc, cost + 100, cur))
                         cmap[nr][nc][cur] = cost + 100
                 else:
-                    if cost + 600 <= cmap[nr][nc][cur]:
+                    if cost + 600 < cmap[nr][nc][cur]:
                         s.append((nr, nc, cost + 600, cur))
                         cmap[nr][nc][cur] = cost + 600
 
