866f9566af4650698d0fa6a414adde9e41e24dfb
김승우 <seungwookim.dev@gmail.com>
2021-09-25 17:01:07 +0900

    [Update] 경주로 건설

--- programmers/2020 카카오 인턴십/경주로 건설.py
+++ programmers/2020 카카오 인턴십/경주로 건설.py
@@ -4,7 +4,7 @@ import math
 
 def solution(board):
     n = len(board)
-    dp = [[[math.inf for _ in range(2)] for _ in range(n)] for _ in range(n)]
+    cmap = [[[math.inf for _ in range(2)] for _ in range(n)] for _ in range(n)]
     s = []
     dx = (0, 0, 1, -1)
     dy = (1, -1, 0, 0)
@@ -23,13 +23,13 @@ def solution(board):
                 cur = 1
             if 0 <= nr < n and 0 <= nc < n and board[nr][nc] == 0:
                 if prev == cur or prev == -1:
-                    if cost + 100 <= dp[nr][nc][cur]:
+                    if cost + 100 <= cmap[nr][nc][cur]:
                         s.append((nr, nc, cost + 100, cur))
-                        dp[nr][nc][cur] = cost + 100
+                        cmap[nr][nc][cur] = cost + 100
                 else:
-                    if cost + 600 <= dp[nr][nc][cur]:
+                    if cost + 600 <= cmap[nr][nc][cur]:
                         s.append((nr, nc, cost + 600, cur))
-                        dp[nr][nc][cur] = cost + 600
+                        cmap[nr][nc][cur] = cost + 600
 
-    answer = min(dp[n - 1][n - 1][0], dp[n - 1][n - 1][1])
+    answer = min(cmap[n - 1][n - 1][0], cmap[n - 1][n - 1][1])
     return answer
