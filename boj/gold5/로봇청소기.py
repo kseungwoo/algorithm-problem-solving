f562adc33de2b97a66bc0bc1a0c2c81ed71afa9e
김승우 <seungwookim.dev@gmail.com>
2021-10-02 05:31:42 +0900

    [Update] 로봇 청소기

--- boj/gold5/로봇청소기.py
+++ boj/gold5/로봇청소기.py
@@ -10,38 +10,20 @@ count = 1
 mp[r][c] = 2
 
 while True:
-    if 0 <= r + dir[(d + 3) % 4][0] < N and 0 <= c + dir[(d + 3) % 4][1] < M and mp[r + dir[(d + 3) % 4][0]][c + dir[(d + 3) % 4][1]] == 0:
-        d = (d + 3) % 4
-        r, c = r + dir[d][0], c + dir[d][1]
-        mp[r][c] = 2
-        count += 1
+    done = False
+    for _ in range(4):
+        if 0 <= r + dir[(d + 3) % 4][0] < N and 0 <= c + dir[(d + 3) % 4][1] < M and mp[r + dir[(d + 3) % 4][0]][c + dir[(d + 3) % 4][1]] == 0:
+            d = (d + 3) % 4
+            r, c = r + dir[d][0], c + dir[d][1]
+            mp[r][c] = 2
+            count += 1
+            done = True
+            break
+        else:
+            d = (d + 3) % 4
+
+    if done:
         continue
-    else:
-        d = (d + 3) % 4
-    if 0 <= r + dir[(d + 3) % 4][0] < N and 0 <= c + dir[(d + 3) % 4][1] < M and mp[r + dir[(d + 3) % 4][0]][c + dir[(d + 3) % 4][1]] == 0:
-        d = (d + 3) % 4
-        r, c = r + dir[d][0], c + dir[d][1]
-        mp[r][c] = 2
-        count += 1
-        continue
-    else:
-        d = (d + 3) % 4
-    if 0 <= r + dir[(d + 3) % 4][0] < N and 0 <= c + dir[(d + 3) % 4][1] < M and mp[r + dir[(d + 3) % 4][0]][c + dir[(d + 3) % 4][1]] == 0:
-        d = (d + 3) % 4
-        r, c = r + dir[d][0], c + dir[d][1]
-        mp[r][c] = 2
-        count += 1
-        continue
-    else:
-        d = (d + 3) % 4
-    if 0 <= r + dir[(d + 3) % 4][0] < N and 0 <= c + dir[(d + 3) % 4][1] < M and mp[r + dir[(d + 3) % 4][0]][c + dir[(d + 3) % 4][1]] == 0:
-        d = (d + 3) % 4
-        r, c = r + dir[d][0], c + dir[d][1]
-        mp[r][c] = 2
-        count += 1
-        continue
-    else:
-        d = (d + 3) % 4
 
     if 0 <= r + dir[(d + 2) % 4][0] < N and 0 <= c + dir[(d + 2) % 4][1] < M and mp[r + dir[(d + 2) % 4][0]][c + dir[(d + 2) % 4][1]] == 2:
         r, c = r + dir[(d + 2) % 4][0], c + dir[(d + 2) % 4][1]
