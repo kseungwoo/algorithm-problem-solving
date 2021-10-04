a648e397977ea03ec4cd2a52bf9db18784214318
김승우 <seungwookim.dev@gmail.com>
2021-10-04 23:56:57 +0900

    [Update] 프렌즈4블록

--- programmers/2018 KAKAO BLIND RECRUITMENT/프렌즈4블록.py
+++ programmers/2018 KAKAO BLIND RECRUITMENT/프렌즈4블록.py
@@ -4,40 +4,33 @@ from collections import deque
 
 def pop(m, n, board):
     new_board = copy.deepcopy(board)
-    isPopped = False
+    isFinished = True
     for r in range(m - 1):
         for c in range(n - 1):
             if board[r][c] == board[r + 1][c] == board[r][c + 1] == board[r + 1][c + 1] != 0:
                 new_board[r][c] = new_board[r + 1][c] = new_board[r][c + 1] = new_board[r + 1][c + 1] = 0
-                isPopped = True
-    return new_board, isPopped
+                isFinished = False
+    return new_board, isFinished
 
 
 def blank_fill(m, n, b):
-    l = deque()
+    dq = deque()
     for c in range(n):
         for r in range(m):
             if b[r][c] != 0:
-                l.append(b[r][c])
-        for _ in range(m - len(l)):
-            l.appendleft(0)
+                dq.append(b[r][c])
+        for _ in range(m - len(dq)):
+            dq.appendleft(0)
         for r in range(m):
-            b[r][c] = l[r]
-        l.clear()
+            b[r][c] = dq[r]
+        dq.clear()
     return b
 
 
 def solution(m, n, board):
-    b = list()
-    for row in board:
-        b.append(list(row))
+    b = [list(row) for row in board]
     while True:
-        b, isPopped = pop(m, n, b)
-        if not isPopped:
-            count = 0
-            for r in range(m):
-                for c in range(n):
-                    if b[r][c] == 0:
-                        count += 1
-            return count
+        b, isFinished = pop(m, n, b)
+        if isFinished:
+            return sum(row.count(0) for row in b)
         b = blank_fill(m, n, b)
