bfbf682b8a9ee07577e5cc776c9bf5cf42f2ec91
김승우 <seungwookim.dev@gmail.com>
2021-11-01 23:37:18 +0900

    [Update] 나무 재테크

--- boj/gold4/나무 재테크.py
+++ boj/gold4/나무 재테크.py
@@ -1,36 +1,36 @@
 import sys
+from collections import deque
 
 N, M, K = map(int, sys.stdin.readline().strip().split())
 A = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(N)]
 B = [[5 for _ in range(N)] for _ in range(N)]
+dy, dx = (-1, -1, -1, 0, 1, 1, 1, 0), (-1, 0, 1, 1, 1, 0, -1, -1)
+
 trees = []
 for _ in range(M):
     x, y, z = map(int, sys.stdin.readline().strip().split())
-    trees.append((y - 1, x - 1, z))
-for _ in range(K):
-    trees.sort(key=lambda t: t[2])
-    for i, v in enumerate(trees):
-        dead = []
-        r, c, old = v[0], v[1], v[2]
-        if old > B[r][c]:
-            dead.append((r, c, old, i))
-        else:
-            B[r][c] -= old
-            trees[i] = (r, c, old + 1)
-    for n in range(len(dead) - 1, -1, -1):
-        r, c, old, i = dead[n][0], dead[n][1], dead[n][2], dead[n][3]
-        del trees[i]
-        B[r][c] += old // 2
-    dy, dx = (-1, -1, -1, 0, 1, 1, 1, 0), (-1, 0, 1, 1, 1, 0, -1, -1)
+    trees.append((x - 1, y - 1, z))
+trees = deque(sorted(trees, key=lambda t: t[2]))
+for k in range(K):
+    dead = []
+    replace_tree = deque()
     for r, c, old in trees:
-        new_trees = []
+        if old > B[r][c] + A[r][c] * k:
+            dead.append((r, c, old))
+        else:
+            replace_tree.append((r, c, old + 1))
+            B[r][c] -= old
+    trees = replace_tree
+    for r, c, old in dead:
+        B[r][c] += old // 2
+    i = 0
+    while i < len(trees):
+        r, c, old = trees[i][0], trees[i][1], trees[i][2]
         if old % 5 == 0:
             for d in range(8):
                 nr, nc = r + dy[d], c + dx[d]
                 if 0 <= nr < N and 0 <= nc < N:
-                    new_trees.append((nr, nc, 1))
-    trees.extend(new_trees)
-    for r in range(N):
-        for c in range(N):
-            B[r][c] += A[r][c]
+                    trees.appendleft((nr, nc, 1))
+                    i += 1
+        i += 1
 print(len(trees))
