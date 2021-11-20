aa7e86fdcfa383572d01e8853c9449bf79d3c3ef
김승우 <seungwookim.dev@gmail.com>
2021-11-20 11:05:04 +0900

    [Add] Is This a Binary Search Tree?

---
+++ HackerRank/Data Structures/Trees/Is This a Binary Search Tree?.py
@@ -0,0 +1,26 @@
+""" Node is defined as
+class node:
+  def __init__(self, data):
+      self.data = data
+      self.left = None
+      self.right = None
+"""
+from collections import deque
+import sys
+
+
+def check_binary_search_tree_(root):
+    q = deque([(root, float('-inf'), float('inf'))])
+    while q:
+        cur, floor, ceil = q.popleft()
+        if cur.left != None:
+            if cur.left.data >= cur.data or cur.left.data <= floor:
+                return False
+            else:
+                q.append((cur.left, floor, min(ceil, cur.data)))
+        if cur.right != None:
+            if cur.right.data <= cur.data or cur.right.data >= ceil:
+                return False
+            else:
+                q.append((cur.right, max(floor, cur.data), ceil))
+    return True
